65082172fc1e3eeae332cb8baf0a92915d8e734b
Icaro Peretti <icaroperetti50@gmail.com>
2022-07-31 13:30:12 -0300

    Create Microsoft.Powershell_profile.ps1

---
+++ Microsoft.Powershell_profile.ps1
@@ -0,0 +1,19 @@
+Import-Module posh-git
+# Import-Module oh-my-posh
+Import-Module PSReadline
+oh-my-posh --init --shell pwsh --config "~/AppData/Local/Programs/oh-my-posh/themes/the-unnamed.omp.json" | Invoke-Expression
+# Set-PoshPrompt -Theme robbyrussel
+
+
+# # Autocomplete
+# Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+# Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+# Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+# Set-PSReadlineOption -HistorySearchCursorMovesToEnd
+
+# Suggestions do PSReadline
+Set-PSReadlineOption -ShowToolTips
+Set-PSReadlineOption -PredictionSource History
+
+
+clear
